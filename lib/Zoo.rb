8ff9611682d47f8f05a4287f312a6cb52a534996
fmwong77 <fongmeiw@gmail.com>
2019-11-05 00:00:40 -0600

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,39 @@
+require_relative 'Animal'
 class Zoo
+    attr_accessor :name, :location
 
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|a| a.zoo == self}
+    end
+
+    def animal_species
+        arr = []
+        Animal.all.select {|a| arr << a.species if a.zoo == self}
+        arr.uniq.join(",")
+        # binding.pry
+        # arr[species].uniq
+    end
+
+    def animal_nicknames
+        arr = []
+        Animal.all.select {|a| arr << a.nickname if a.zoo == self}
+        arr
+    end
+
+    def self.find_by_location(location)
+        @@all.select {|z| z.location == location}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,18 @@ require 'pry'
 
 
 #Test your code here
+zoo_austin = Zoo.new("Austin Zoo", "Austin")
+zoo_Houston = Zoo.new("Houston Zoo", "Houston")
 
+spotty = Animal.new("Dog", 15, "spotty")
+spotty.zoo = zoo_austin
+olive = Animal.new("Dog", 30, "olive")
+olive.zoo = zoo_austin
+
+mushy = Animal.new("Cat", 5, "mushy")
+mushy.zoo = zoo_Houston
+stout = Animal.new("Guinea Pig", 5, "stout")
+stout.zoo = zoo_Houston
 
 binding.pry
 puts "done"
