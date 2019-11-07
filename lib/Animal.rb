1abda2e88112ac56e8e19443bc55cb0aa556a832
crayray <rach.fuerst@gmail.com>
2019-11-07 09:16:45 -0600

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,36 @@
 class Animal
+    attr_accessor :weight, :zoo
+    attr_reader :nickname, :species
+
+    @@all_animals=[]
+    def initialize(nickname, species, weight, zoo)
+        @nickname= nickname
+        @species= species
+        @weight=weight
+        @zoo=zoo
+
+        @@all_animals << self
+    end
+
+    def self.all
+        @@all_animals
+    end
+
+    def find_nickname # NEED HELP WITH THIS ONE -- > returns:  [  ]
+       self.nickname
+    end
+
+    def weights # NEED HELP WITH THIS ONE TOO
+        self.weight
+    end
+
+    def zoo_name ### SAME
+        self.zoo.name
+    end
+
+
+    def self.find_by_species(species)
+        Animal.all.select {|animal| animal.species == species}
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,38 @@
 class Zoo
+    attr_accessor :name
+    attr_reader :location
+
+    @@all_zoos= []
+    def initialize(name, location)
+        @name=name
+        @location= location
+        @@all_zoos << self
+
+    end
+
+    def add_animal(nickname,species, weight, zoo) #is this correct!? Does an animal need to keep track of the zoo or the other way around?
+        Animal.new(nickname, species, weight, self)
+        
+    end
+
+    def self.all
+        @@all_zoos
+    end
+
+    def animals
+        Animal.all.select {|animals|animals.zoo == self}
+    end
+
+    def animal_species
+        animals.map{ |animal| animal.species}
+    end
+
+    def animal_nicknames
+        animals.map {|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        Zoo.all.select {|zoos| zoos.location == location}
+    end
 
 end
